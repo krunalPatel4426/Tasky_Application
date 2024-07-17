dca3673759224792758ddab8f2a1edd48c804084
krunalPatel4426 <YOUR_EMAIL>
2024-07-17 10:02:07 +0530

    day 3 commit

---
+++ js folder/index.js
@@ -0,0 +1,8 @@
+const state = {
+    taskList: [],
+}
+
+const taskContent = document.querySelector(".taskContent")
+const taskModalBody = document.querySelector(".taskModalBody")
+console.log(taskContent);
+console.log(taskModalBody);
